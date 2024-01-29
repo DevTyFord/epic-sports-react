279c98a6cf920add1c8cbf2dd3756eb49eebd15b
Tyler <contact@epicsports.media>
2024-01-28 20:21:40 -0600

    Fixes @Media Menu collapse issue

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -12,9 +12,10 @@ function Nav () {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href='#'/>
+              <div className='navbar-brand'>
                 <img src={ Logo }></img>
-            <button className="navbar-toggler" type="button" data-bs-toggle="dropdown" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              </div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
